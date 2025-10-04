900146f7440d67a034976e3987e24ecfce5a58c6
Cristian Florez <cristianflorez873@gmail.com>
2025-10-04 09:16:51 -0500

    add img

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,8 @@ import sequelize from "../config/database.js";
 const User = sequelize.define("User", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true }
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  img:{type:DataTypes.img, allowNull:true}
 }, {
   timestamps: true,
   tableName: "users"
